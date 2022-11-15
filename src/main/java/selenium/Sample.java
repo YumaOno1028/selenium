f4e356dc252ddd3e72079f84c2384c373725ee09
Yuma Ono <yuma.ono@rakus-partners.co.jp>
2022-11-15 10:29:57 +0900

    sample

--- src/main/java/selenium/Sample.java
+++ src/main/java/selenium/Sample.java
@@ -1,6 +1,9 @@
 package selenium;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -12,6 +15,10 @@ public class Sample {
 		WebDriver driver = new ChromeDriver();
 		
 		driver.get("http://www.google.co.jp");
+		WebElement element = driver.findElement(By.cssSelector("body > div.L3eUgb > div.o3j99.ikrT4e.om7nvf > form > div:nth-child(1) > div.A8SBwf > div.RNNXgb > div > div.a4bIc > input"));
+		element.sendKeys("CSSセレクタとは？");
+		element.sendKeys(Keys.ENTER);
+		driver.close();
 		
 	}
 
